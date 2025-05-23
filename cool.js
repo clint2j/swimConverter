86d585f84cffb1b8ee7551592222493331386e5b
Clint Hart <clinthart21@tamu.edu>
2025-05-23 12:34:48 -0600

    Revised maifest and fixed bug with 400 IM

--- cool.js
+++ cool.js
@@ -1,322 +1,150 @@
-var fincre;
-var incre;
-var intim;
-var ffactor;
-var ntime;
-var ntime1;
-var ntime2;
-var ntime3;
-var stroke;
-var dis;
-var bob;
-var fin;
-var fin1 = 0;
-var fin2 = 0;
-var fin3 = 0;
-var type;
-document.getElementById("but").onclick = function() {
-document.getElementById("value").style.display = "block";
-ntime1 = document.getElementById("ntime_1").value;
-ntime2 = document.getElementById("ntime_2").value;
-ntime = Number(ntime1)*60 + Number(ntime2);
-type = document.getElementById("stroe_2").value;
-switch (type) {
-case "scy_lcm":
-conver1();
-break;
-case "scy_scm":
-conver2();
-break;
-case "scm_scy":
-conver3();
-break;
-case "scm_lcm":
-conver5();
-break;
-case "lcm_scm":
-conver6();
-break;
-default:
-conver4();
-}
-}
-//scy_lcm
-function conver1() {
-stroke = document.getElementById("stroke_1").value;
-dis = document.getElementById("dis_1").value;
-ffactor = 1.11;
-switch (stroke) {
-case "fr":
-incre = .8;
-break;
-case "bk":
-incre = .6;
-break;
-case "br":
-incre = 1;
-break;
-case "fl":
-incre = .7;
-break;
-case "im":
-incre = .8;
-break;
-default:
-console.log("error 1");
+document.getElementById("but").onclick = () => {
+  document.getElementById("output").style.display = "block";
+  const minsInput = document.getElementById("mins_inputted").value.trim();
+  const secsInput = document.getElementById("secs_inputted").value.trim();
+
+  let mins = parseFloat(minsInput);
+  let secs = parseFloat(secsInput);
+
+  if ((minsInput === "" && secsInput === "") || mins < 0 || secs < 0) {
+    document.getElementById("output_value").innerHTML = "NaN";
+    return;
+  }
+
+  if (isNaN(mins)) {
+    mins = 0;
+  }
+
+  if (isNaN(secs)) {
+    secs = 0;
+  }
+
+  const totalSeconds = mins * 60 + secs;
+
+  const eventInput = document.getElementById("event")?.value ?? "";
+  const eventMatch = eventInput.match(/^(\d+)([a-zA-Z]+)$/);
+
+  if (!eventMatch) {
+    console.error("Invalid event format");
+    return;
+  }
+
+  const distance = Number(eventMatch[1]);
+  const stroke = eventMatch[2];
+
+  const conversionValue = document.getElementById("conversion")?.value ?? "";
+  const inputType = conversionValue.slice(0, 3);
+  const outputType = conversionValue.slice(4);
+
+  const convertedTime = convert(
+    totalSeconds,
+    distance,
+    stroke,
+    inputType,
+    outputType
+  );
+  document.getElementById("output_value").innerHTML =
+    formattedTime(convertedTime);
 };
-switch (dis) {
-case "50":
-intim = 1;
-break;
-case "100":
-intim = 2;
-break;
-case "200":
-intim = 4;
-break;
-case "500":
-if (stroke !== "fr") {
-window.alert("Warning: convertion was done for 500 free");
-}
-intim = 0;
-ffactor = 0.8925;
-break;
-case "1000":
-if (stroke !== "fr") {
-window.alert("Warning: convertion was done for 1000 free");
-}
-intim = 0;
-ffactor = 0.8925;
-break;
-case "1650":
-if (stroke !== "fr") {
-window.alert("Warning: convertion was done for 1650 free");
-}
-intim = 0;
-ffactor = 1.02;
-break;
-default:
-console.log("error 2");
+
+const convert = (totalSeconds, distance, stroke, inputType, outputType) => {
+  let outputTime = 0;
+
+  if ((distance === 400 && stroke == "fr") || distance === 800) {
+    outputTime = totalSeconds;
+    if (inputType === "scm") {
+      outputTime += (6.4 * distance) / 400;
+    }
+    if (outputType === "scy") {
+      outputTime /= 0.8925;
+    }
+    if (inputType === "scy") {
+      outputTime *= 0.8925;
+    }
+    if (outputType === "scm") {
+      outputTime -= (6.4 * distance) / 400;
+    }
+    return outputTime;
+  }
+
+  if (distance === 1650) {
+    outputTime = totalSeconds;
+    if (inputType === "scm") {
+      outputTime += 24;
+    }
+    if (outputType === "scy") {
+      outputTime /= 1.02;
+    }
+    if (inputType === "scy") {
+      outputTime *= 1.02;
+    }
+    if (outputType === "scm") {
+      outputTime -= 24;
+    }
+    return outputTime;
+  }
+
+  if (inputType == "lcm_scy") {
+    switch (stroke) {
+      case "fr":
+        totalSeconds -= 0.8 * (distance / 50);
+        break;
+      case "bk":
+        totalSeconds -= 0.6 * (distance / 50);
+        break;
+      case "br":
+        totalSeconds -= 1 * (distance / 50);
+        break;
+      case "fl":
+        totalSeconds -= 0.7 * (distance / 50);
+        break;
+      case "im":
+        totalSeconds -= 0.8 * (distance / 50);
+        break;
+    }
+  }
+
+  if (inputType == "scy") {
+    outputTime = totalSeconds * 1.11;
+  } else {
+    outputTime = totalSeconds / 1.11;
+  }
+
+  if (inputType == "scy" && outputType == "lcm") {
+    switch (stroke) {
+      case "fr":
+        outputTime += 0.8 * (distance / 50);
+        break;
+      case "bk":
+        outputTime += 0.6 * (distance / 50);
+        break;
+      case "br":
+        outputTime += 1 * (distance / 50);
+        break;
+      case "fl":
+        outputTime += 0.7 * (distance / 50);
+        break;
+      case "im":
+        outputTime += 0.8 * (distance / 50);
+        break;
+    }
+  }
+
+  return outputTime;
 };
-bob = incre*intim+ffactor*ntime;
-convert()
-}
-//scy_scm
-function conver2() {
-stroke = document.getElementById("stroke_1").value;
-dis = document.getElementById("dis_1").value;
-var num = 0;
-switch (dis) {
-  case "500":
-  num = 1.143;
-  break;
-  case "1000":
-  num = 1.143;
-  break;
-  case "1650":
-  num = 1.003;
-  break;
-  default:
-  num = .9009009009;
-}
-bob = ntime/num;
-convert()
-}
-//scm_scy
-function conver3() {
-stroke = document.getElementById("stroke_1").value;
-dis = document.getElementById("dis_1").value;
-var num = 0;
-switch (dis) {
-  case "500":
-  num = 1.143;
-  break;
-  case "1000":
-  num = 1.143;
-  break;
-  case "1650":
-  num = 1.003;
-  break;
-  default:
-  num = .9009009009;
-}
-bob = ntime*num;
-convert()
-}
-//lcm_scy
-function conver4() {
-stroke = document.getElementById("stroke_1").value;
-dis = document.getElementById("dis_1").value;
-ffactor = 1.11;
-switch (stroke) {
-case "fr":
-incre = .8;
-break;
-case "bk":
-incre = .6;
-break;
-case "br":
-incre = 1;
-break;
-case "fl":
-incre = .7;
-break;
-case "im":
-incre = .8;
-break;
-default:
-console.log("error 1");
+
+const formattedTime = (time) => {
+  const hours = Math.floor(time / 3600);
+  const minutes = Math.floor((time % 3600) / 60);
+  const seconds = time % 60;
+
+  if (hours > 0) {
+    return `${hours}:${String(minutes).padStart(2, "0")}:${String(
+      seconds
+    ).padStart(2, "0")}`;
+  } else if (minutes > 0) {
+    return `${minutes}:${String(seconds).padStart(2, "0")}`;
+  } else {
+    return `${seconds}`;
+  }
 };
-switch (dis) {
-case "50":
-intim = 1;
-break;
-case "100":
-intim = 2;
-break;
-case "200":
-intim = 4;
-break;
-case "500":
-if (stroke !== "fr") {
-window.alert("Warning: convertion was done for 500 free");
-}
-intim = 0;
-ffactor = 0.8925;
-break;
-case "1000":
-if (stroke !== "fr") {
-window.alert("Warning: convertion was done for 1000 free");
-}
-intim = 0;
-ffactor = 0.8925;
-break;
-case "1650":
-if (stroke !== "fr") {
-window.alert("Warning: convertion was done for 1650 free");
-}
-intim = 0;
-ffactor = 1.02;
-break;
-default:
-console.log("error 2");
-};
-bob = (ntime-incre*intim)/ffactor;
-convert();
-}
-//scm_lcm
-function conver5() {
-stroke = document.getElementById("stroke_1").value;
-dis = document.getElementById("dis_1").value;
-ffactor = 1;
-switch (stroke) {
-case "fr":
-incre = .8;
-break;
-case "bk":
-incre = .6;
-break;
-case "br":
-incre = 1;
-break;
-case "fl":
-incre = .7;
-break;
-case "im":
-incre = .8;
-break;
-default:
-console.log("error 1");
-};
-switch (dis) {
-case "50":
-intim = 1;
-break;
-case "100":
-intim = 2;
-break;
-case "200":
-intim = 4;
-break;
-case "500":
-intim = 8;
-break;
-case "1000":
-intim = 16;
-break;
-case "1650":
-intim = 30;
-break;
-default:
-console.log("error 2");
-};
-bob = incre*intim+ffactor*ntime;
-convert();
-}
-//lcm_scm
-function conver6() {
-stroke = document.getElementById("stroke_1").value;
-dis = document.getElementById("dis_1").value;
-ffactor = 1;
-switch (stroke) {
-case "fr":
-incre = .8;
-break;
-case "bk":
-incre = .6;
-break;
-case "br":
-incre = 1;
-break;
-case "fl":
-incre = .7;
-break;
-case "im":
-incre = .8;
-break;
-default:
-console.log("error 1");
-};
-switch (dis) {
-case "50":
-intim = 1;
-break;
-case "100":
-intim = 2;
-break;
-case "200":
-intim = 4;
-break;
-case "500":
-intim = 8;
-break;
-case "1000":
-intim = 16;
-break;
-case "1650":
-intim = 30;
-break;
-default:
-console.log("error 2");
-};
-bob = (ntime-incre*intim)/ffactor;
-convert();
-}
-function convert() {
-fin1 = 0;
-fin2 = 0;
-fin3 = "";
-fin2 = Number(bob);
-while (fin2/60 >= 1) {
-fin2 -= 60;
-fin1 ++;
-}
-if (fin2 < 10) {
-fin3 = "0";
-} 
-if (fin1 !== 0) {
-document.getElementById("value_1").innerHTML= fin1 + ":" + fin3 + fin2;
-} else {
-document.getElementById("value_1").innerHTML= fin3 + fin2;
-}
-}
